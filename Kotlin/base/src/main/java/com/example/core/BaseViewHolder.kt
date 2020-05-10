c8b009c8a68e1c7044cf4f4a04d49b7fe5afa71d
mac <mac@mofunsky.com>
2020-05-10 11:45:13 +0800

    改造 剩余 kotlin

---
+++ Kotlin/base/src/main/java/com/example/core/BaseViewHolder.kt
@@ -0,0 +1,28 @@
+package com.example.core
+
+import android.annotation.SuppressLint
+import android.view.View
+import android.widget.TextView
+import androidx.annotation.IdRes
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import java.util.*
+
+abstract class BaseViewHolder : ViewHolder {
+    constructor(itemView: View) : super(itemView)
+
+    @SuppressLint("UseSparseArrays")
+    private val viewHashMap: MutableMap<Int, View?> = HashMap()
+
+    protected open fun <T : View?> getView(@IdRes id: Int): T? {
+        var view = viewHashMap[id]
+        if (view == null) {
+            view = itemView.findViewById(id)
+            viewHashMap[id] = view
+        }
+        return view as T?
+    }
+
+    protected open fun setText(@IdRes id: Int, text: String?) {
+        (getView<View>(id) as TextView).text = text
+    }
+}
